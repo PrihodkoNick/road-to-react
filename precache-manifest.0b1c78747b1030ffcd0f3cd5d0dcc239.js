ba6d4d2ce7e96daf9de618e4077bdfb623a906db
Prihodko Nick <prihodkonn@gmail.com>
2020-05-05 18:00:18 +0300

    Updates

---
+++ precache-manifest.0b1c78747b1030ffcd0f3cd5d0dcc239.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48d0a974b44478ca8689b070aa835db",
+    "url": "/road-to-react/index.html"
+  },
+  {
+    "revision": "2aeecf2e880783488274",
+    "url": "/road-to-react/static/css/main.e9d84706.chunk.css"
+  },
+  {
+    "revision": "e88b6117305bd683e016",
+    "url": "/road-to-react/static/js/2.78f0caf9.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/road-to-react/static/js/2.78f0caf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aeecf2e880783488274",
+    "url": "/road-to-react/static/js/main.e157491f.chunk.js"
+  },
+  {
+    "revision": "f165d25fed17ef3003d9",
+    "url": "/road-to-react/static/js/runtime-main.44594de8.js"
+  }
+]);
